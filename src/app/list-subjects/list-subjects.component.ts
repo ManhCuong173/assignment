7e057519f1875c31c53679230dd8468dcb22865b
ManhCuong173 <daomanhcuong173@gmail.com>
2019-11-10 22:51:16 +0700

    Assignment giai doan 2 va 3 fix loi

--- src/app/list-subjects/list-subjects.component.ts
+++ src/app/list-subjects/list-subjects.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit,  ElementRef } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { ListSubjectService } from './../service/list-subject.service'
 import { ListQuestionService } from './../service/list-question.service'
-import {ListUserService} from './../service/list-user.service' 
-import {Router} from '@angular/router'
-import {CookieService} from 'ngx-cookie-service'
-import {CheckLoginService} from './../middleware/check-login.service'
+import { ListUserService } from './../service/list-user.service'
+import { Router } from '@angular/router'
+import { CookieService } from 'ngx-cookie-service'
+import { CheckLoginService } from './../middleware/check-login.service'
 @Component({
   selector: 'app-list-subjects',
   templateUrl: './list-subjects.component.html',
@@ -19,17 +19,17 @@ export class ListSubjectsComponent implements OnInit {
   itemPages = [];
   itemStart = 1;
   itemEnd = 3;
-  user : any = {};
+  user: any = {};
   users: any = [];
   userBirthday: Date;
 
   //Change Password Variables
   password: any;
-  newpassword:any;
+  newpassword: any;
   renewpassword: any;
 
-  constructor(private _listSubject: ListSubjectService, private _listQuestion: ListQuestionService, private _listUser: ListUserService, private _elementRef : ElementRef, private routerNavigate: Router, private _cookie: CookieService, private _checkLoginService: CheckLoginService) { }
-  
+  constructor(private _listSubject: ListSubjectService, private _listQuestion: ListQuestionService, private _listUser: ListUserService, private _elementRef: ElementRef, private routerNavigate: Router, private _cookie: CookieService, private _checkLoginService: CheckLoginService) { }
+
   listSubjectComponent = this._listSubject.listSubjects;
   totalPages = Math.ceil(this.listSubjectComponent.length / this.itemPerPage);
   isPending = false;
@@ -38,30 +38,30 @@ export class ListSubjectsComponent implements OnInit {
     this._checkLoginService.isLogin(this._cookie.get('user-id'));
 
     //Get query param 
-      this.idUser = this._cookie.get('user-id');
+    this.idUser = this._cookie.get('user-id');
 
-    for(let i=1; i<=this.totalPages; i++) {
+    for (let i = 1; i <= this.totalPages; i++) {
       this.itemPages.push(i);
-    } 
+    }
 
     this._listUser.getJSON(this.idUser).subscribe(data => {
       this.user = data;
       this.userBirthday = this.user.birthday;
-      this.user.gender?this.user.gender='male':this.user.gender='female';
+      this.user.gender ? this.user.gender = 'male' : this.user.gender = 'female';
       this._elementRef.nativeElement.querySelector(`#${this.user.gender}`).checked = true;
     })
   }
-  showListMode(){
+  showListMode() {
     this.isShow = !this.isShow;
   }
   //Move to prev page
-  prevPage(){
-    if(this.curPage == 1) {} 
+  prevPage() {
+    if (this.curPage == 1) { }
     else this.curPage -= 1;
   }
   //Move to next page
   nextPage() {
-    if(this.curPage == this.totalPages) return
+    if (this.curPage == this.totalPages) return
     else {
       this.curPage += 1;
     }
@@ -74,15 +74,15 @@ export class ListSubjectsComponent implements OnInit {
   //Cal total page
   calTotalPage() {
     let array = [];
-    if(this.curPage > this.itemEnd) {
+    if (this.curPage > this.itemEnd) {
       this.itemStart += 1;
       this.itemEnd += 1;
     }
-    else if(this.curPage < this.itemStart) {
+    else if (this.curPage < this.itemStart) {
       this.itemEnd -= 1;
       this.itemStart -= 1;
     }
-    for(let i=this.itemStart; i<=this.itemEnd; i++) {
+    for (let i = this.itemStart; i <= this.itemEnd; i++) {
       array.push(i);
     };
     return array
@@ -90,15 +90,15 @@ export class ListSubjectsComponent implements OnInit {
 
   //Change Password
   changePassword() {
-    if(this.password == undefined || this.password == '') alert('Bạn chưa nhập mật khẩu cũ')
+    if (this.password == undefined || this.password == '') alert('Bạn chưa nhập mật khẩu cũ')
     else {
-      if(this.password != this.user.password[this.user.password.length - 1] && this.password != undefined) alert('Mật khẩu cũ không đúng')
-      else{
-        if(this.newpassword == undefined || this.newpassword == '') alert('Bạn chưa nhập mật khẩu mới');
-        else{
-          if(this.renewpassword == undefined || this.renewpassword == '') alert('Bạn chưa nhập mật khẩu xác minh');
-          else{
-            if(this.newpassword != this.renewpassword) alert("Mật khẩu xác minh không trùng khớp");
+      if (this.password != this.user.password[this.user.password.length - 1] && this.password != undefined) alert('Mật khẩu cũ không đúng')
+      else {
+        if (this.newpassword == undefined || this.newpassword == '') alert('Bạn chưa nhập mật khẩu mới');
+        else {
+          if (this.renewpassword == undefined || this.renewpassword == '') alert('Bạn chưa nhập mật khẩu xác minh');
+          else {
+            if (this.newpassword != this.renewpassword) alert("Mật khẩu xác minh không trùng khớp");
             else {
               this.user.password.push(this.newpassword);
               this.changeInfo();
@@ -112,7 +112,7 @@ export class ListSubjectsComponent implements OnInit {
   //Chose subject
   chooseSubject(idSubject) {
     this._listQuestion.idSubject = idSubject;
-    this.routerNavigate.navigate(['user/quiz'], {queryParams: {userId: `${this.user.id}`, 'quizId': `${idSubject}`}})
+    this.routerNavigate.navigate(['user/quiz'], { queryParams: { userId: `${this.user.id}`, 'quizId': `${idSubject}` } })
   }
 
   //Check gender function
@@ -121,27 +121,17 @@ export class ListSubjectsComponent implements OnInit {
   }
 
   //Change infomartion of user
-  changeInfo(){
-    let isUsed = false;
-    this._listUser.allUser().subscribe(data => {
-      this.users = data;
-      this.users.forEach(element => {
-        if(this.user.email == element.email) isUsed = !isUsed;
-      });
-    if(isUsed) alert("Email này đã có người sử dụng");
-    else {
-      if(this.user.gender=='male') this.user.gender = true
-          else this.user.gender = false;
-          this.user.birthday = this.userBirthday;
-          this._listUser.changeInfo(this.user).subscribe(data => {
-            alert('Thay đổi thông tin thành công');
-          });
-    }
-    })
+  changeInfo() {
+    if (this.user.gender == 'male') this.user.gender = true;
+    else this.user.gender = false;
+    this.user.birthday = this.userBirthday;
+    this._listUser.changeInfo(this.user).subscribe(data => {
+      alert('Thay đổi thông tin thành công');
+    });
   }
 
   //Sign out 
-  signOut(){
+  signOut() {
     this._cookie.delete('user-id');
   }
 }
